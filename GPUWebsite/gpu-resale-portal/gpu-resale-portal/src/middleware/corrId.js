c5951b4dc1956bfe0b8d8e6966349033803b421c
gigidyjnb-dev <gigidyjnb@gmail.com>
2025-12-29 11:27:51 -0700

    Remove duplicate nested folder

--- GPUWebsite/gpu-resale-portal/gpu-resale-portal/src/middleware/corrId.js
+++
@@ -1,8 +0,0 @@
-import { randomUUID } from 'crypto';
-
-export function withCorrId(req, res, next) {
-  req.corrId = req.headers['x-corr-id'] || randomUUID();
-  res.setHeader('x-corr-id', req.corrId);
-  next();
-}
-
--- GPUWebsite/gpu-resale-portal/gpu-resale-portal/src/utils/log.js
+++
@@ -1,9 +0,0 @@
-export function log(corrId, stage, data) {
-  console.log(JSON.stringify({
-    ts: Date.now(),
-    corrId,
-    stage,
-    ...data
-  }));
-}
-
